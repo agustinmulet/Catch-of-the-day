a469203fc4fd03701950603c35e820dac298de35
Agustin <agustin.mulet@gmail.com>
2019-01-23 11:21:06 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Catch of the day
+
+From the [React for beginners](https://reactforbeginners.com) course made by [Wes Bos](https://wesbos.com) using React + Firebase 🔥
---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = (props) => (
+    <header className="top">
+            <h1>Not found!!!</h1>
+    </header>
+)
+
+export default NotFound;
