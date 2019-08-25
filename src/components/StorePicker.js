906df3203b2c9fd770873a6635e1f395cf5a5224
Agustin Mulet <agustin.mulet@gmail.com>
2019-08-25 16:33:24 -0300

    Updated packages for security and a couple of fixes to errors provoked by the update

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,38 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { getFunName } from '../helpers';
+import React from "react";
+import PropTypes from "prop-types";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-    myInput = React.createRef();
-    static propTypes = {
-        history: PropTypes.object
-    }
+  myInput = React.createRef();
+  static propTypes = {
+    history: PropTypes.object
+  };
 
-    goToStore = (event) => {
-        // 1. Stop the form from submitting
-        event.preventDefault();
-        // 2. get the text from that input
-        const storeName = this.myInput.value.value;
-        // 3. Change the page to /store/loquesea
-        this.props.history.push(`/store/${storeName}`)
-    }
-    render() {
-        return (
-            <form onSubmit={this.goToStore} className="store-selector">
-                { /* Comentario va entre llaves y tiene que estar dentro
-                del elemento */ }
-                <h2>Please enter a store</h2>
-                <input 
-                  type="text" 
-                  ref={this.myInput}
-                  required 
-                  placeholder="Store Name" 
-                  defaultValue={getFunName()}
-                />
-                <button type="submit">Visit store →</button>
-            </form>
-        )
-    }
+  goToStore = event => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. get the text from that input
+    const storeName = this.myInput.current.value;
+    // 3. Change the page to /store/loquesea
+    this.props.history.push(`/store/${storeName}`);
+  };
+  render() {
+    return (
+      <form onSubmit={this.goToStore} className="store-selector">
+        {/* Comentario va entre llaves y tiene que estar dentro
+                del elemento */}
+        <h2>Please enter a store</h2>
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit store →</button>
+      </form>
+    );
+  }
 }
 
 export default StorePicker;
